4f1aaee979d87902fa25da52fe3642e8136885eb
Dana Christo <christo.dana@gmail.com>
2017-03-10 11:48:13 -0500

    Main Menu complete

--- Puppy Petter/Assets/Scripts/ButtonHighlight.cs
+++ Puppy Petter/Assets/Scripts/ButtonHighlight.cs
@@ -6,41 +6,24 @@ using UnityEngine.UI;
 
 public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler {
     public Image corgi;
-    Image inst;
+
+    void Start () {
+        corgi.enabled = false;
+    }
 
 	public void OnPointerEnter (PointerEventData eventData) {
-        // New position
-        Vector3 spawnPosition = new Vector3 (
-            transform.position.x, 
-            transform.position.y + 2.0f, 
-            0.0f);
-
-        // No rotation
-        Quaternion spawnRotation = Quaternion.identity;
-
-        // Display and destroy heart
-        Instantiate (corgi, spawnPosition, spawnRotation);
+        corgi.enabled = true;
     }
 
     public void OnSelect (BaseEventData eventData) {
-        // New position
-        Vector3 spawnPosition = new Vector3 (
-            transform.position.x, 
-            transform.position.y + 2.0f, 
-            0.0f);
-
-        // No rotation
-        Quaternion spawnRotation = Quaternion.identity;
-
-        // Display and destroy heart
-        inst = Instantiate (corgi, spawnPosition, spawnRotation);
+        corgi.enabled = true;
     }
 
 	public void OnPointerExit (PointerEventData eventData) {
-        Destroy (inst);
+        corgi.enabled = false;
     }
 
     public void OnDeselect (BaseEventData eventData) {
-        Destroy (inst);
+        corgi.enabled = false;
     }
 }
